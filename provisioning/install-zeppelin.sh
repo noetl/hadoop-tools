dbb9eaef1cefa919e10105e7593c679cae3b24ed
Alexander Pivovarov <apivovarov@gmail.com>
2016-02-22 01:31:09 -0800

    Add install-zeppelin.sh

--- provisioning/install-zeppelin.sh
+++ provisioning/install-zeppelin.sh
@@ -18,9 +18,16 @@ tar xzf zeppelin-0.5.6-incubating-bin-all.tgz
 mv zeppelin-0.5.6-incubating-bin-all zeppelin
 rm -rf zeppelin-0.5.6-incubating-bin-all.tgz
 
-mkdir -p /var/log/zeppelin /var/zeppelin/notebook /var/zeppelin/webapps
-chown -R hadoop:hadoop /var/log/zeppelin /var/zeppelin
+echo "Configuring dirs for Zeppelin...."
+mkdir -p /var/log/zeppelin /var/zeppelin/notebook /var/zeppelin/webapps /usr/lib/zeppelin/run
+chown -R hadoop:hadoop /var/log/zeppelin /var/zeppelin /usr/lib/zeppelin/conf /usr/lib/zeppelin/run
 su - hadoop -c 'cp -R /usr/lib/zeppelin/notebook/* /var/zeppelin/notebook/'
+
+echo "Configuring libs for Zeppelin...."
+mkdir /usr/lib/zeppelin/libs3
+cp /usr/lib/zeppelin/lib/aws*.jar /usr/lib/zeppelin/libs3/
+cp /usr/lib/hadoop/share/hadoop/tools/lib/hadoop-aws-*.jar /usr/lib/zeppelin/libs3/
+cp /usr/lib/hadoop/share/hadoop/tools/lib/jets3t*.jar /usr/lib/zeppelin/libs3/
 
 echo "Configuring Zeppelin...."
 
@@ -33,6 +40,7 @@ export ZEPPELIN_WAR_TEMPDIR=/var/zeppelin/webapps
 export ZEPPELIN_NOTEBOOK_DIR=/var/zeppelin/notebook
 export SPARK_HOME=/usr/lib/spark
 export HADOOP_CONF_DIR=/usr/lib/hadoop/etc/hadoop
+export CLASSPATH=/usr/lib/zeppelin/libs3/*
 EOL
 
 echo "Configuring Zeppelin done"
