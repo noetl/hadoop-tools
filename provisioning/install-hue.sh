4c8ac68b1fd90cfc704366d83266767d6f39da82
Alexander Pivovarov <alex.pivovarov@radius.com>
2016-03-28 10:12:52 -0700

    Add hue.ini

--- provisioning/install-hue.sh
+++ provisioning/install-hue.sh
@@ -31,6 +31,37 @@ PREFIX=/usr/lib make install
 useradd hue
 chown -R hue /usr/lib/hue
 
+cat > /usr/lib/hue/desktop/conf/hue.ini << EOL
+[desktop]
+  secret_key=219d9744d9b801633f174715dc22c3c9f6e12929
+  http_host=0.0.0.0
+  http_port=8888
+  time_zone=America/Toronto
+  django_debug_mode=false
+  http_500_debug_mode=false
+
+[hadoop]
+  [[hdfs_clusters]]
+    [[[default]]]
+      fs_defaultfs=hdfs://${MASTER}:8020
+      hadoop_conf_dir=/usr/lib/hadoop/etc/hadoop
+  [[yarn_clusters]]
+    [[[default]]]
+      resourcemanager_host=${MASTER}
+      submit_to=True
+  [[mapred_clusters]]
+    [[[default]]]
+      submit_to=False
+
+[beeswax]
+  hive_server_host=${MASTER}
+  hive_server_port=10000
+  hive_conf_dir=/usr/lib/hive/conf
+  use_get_log_api=false
+  download_row_limit=1000000
+
+EOL
+
 echo "Starting HUE..."
 su - hue -c 'nohup /usr/lib/hue/build/env/bin/supervisor > /tmp/hue-supervisor.out < /dev/null &'
 echo "done"
